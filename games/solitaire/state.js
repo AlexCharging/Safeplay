0e30a63f668817ae0a735663725ea6d3184b5d56
AlexCharging <alex.goodwin2@icloud.com>
2026-04-24 12:48:36 -0400

    Create state.js

---
+++ games/solitaire/state.js
@@ -0,0 +1,45 @@
+export const createInitialState = () => {
+  const suits = ["♥", "♦", "♣", "♠"];
+  const values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+
+  function shuffle(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+    return deck;
+  }
+
+  const deck = [];
+
+  for (let s = 0; s < suits.length; s++) {
+    for (let v = 0; v < values.length; v++) {
+      deck.push({
+        suit: suits[s],
+        value: values[v],
+        color: (s < 2) ? "red" : "black",
+        faceUp: false
+      });
+    }
+  }
+
+  shuffle(deck);
+
+  const tableau = [[],[],[],[],[],[],[]];
+
+  for (let i = 0; i < 7; i++) {
+    for (let j = 0; j <= i; j++) {
+      let card = deck.pop();
+      if (j === i) card.faceUp = true;
+      tableau[i].push(card);
+    }
+  }
+
+  return {
+    tableau,
+    stock: deck,
+    waste: [],
+    foundations: [[],[],[],[]],
+    selected: null
+  };
+};
