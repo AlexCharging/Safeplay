4081d9731377009c9c3424749ee0a8b790f6cae9
AlexCharging <alex.goodwin2@icloud.com>
2026-04-24 13:23:51 -0400

    Update drag.js

--- games/solitaire/drag.js
+++ games/solitaire/drag.js
@@ -1,1 +1,16 @@
+export function enableDrag(el, onDrop) {
+  let dragged = null;
 
+  el.addEventListener("mousedown", (e) => {
+    dragged = el;
+    el.classList.add("moving");
+  });
+
+  document.addEventListener("mouseup", () => {
+    if (dragged) {
+      onDrop();
+      dragged.classList.remove("moving");
+      dragged = null;
+    }
+  });
+}
