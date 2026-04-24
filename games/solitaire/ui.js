b4856020dfe425bda33a0fc37f2dcbae6bfb2ef7
AlexCharging <alex.goodwin2@icloud.com>
2026-04-24 12:03:13 -0400

    Update ui.js

--- games/solitaire/ui.js
+++ games/solitaire/ui.js
@@ -1,1 +1,25 @@
+function render() {
+  renderTableau();
+}
 
+function renderTableau() {
+  const columns = document.querySelectorAll(".column");
+
+  columns.forEach((col, i) => {
+    col.innerHTML = "";
+
+    gameState.tableau[i].forEach(card => {
+      const el = document.createElement("div");
+      el.className = "card";
+
+      if (!card.faceUp) {
+        el.classList.add("back");
+        el.textContent = "";
+      } else {
+        el.textContent = `${card.value}${card.suit}`;
+      }
+
+      col.appendChild(el);
+    });
+  });
+}
